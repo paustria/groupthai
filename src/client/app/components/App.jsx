4489061e2cdfba6208e6ffd22303227ec3eced1e
Phily Austria <austriaphily@gmail.com>
2017-05-12 21:34:21 -0700

    feat(privateRoute) : Added privateRoute feature.

--- src/client/app/components/App.jsx
+++ src/client/app/components/App.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import {
+    Redirect,
     BrowserRouter,
     Switch,
-    Route,
-    Link
-} from 'react-router-dom'
+    Route
+} from 'react-router-dom';
+
 import Home from './Home';
 import Nav from './Nav';
 import Login from './Login';
 import Dashboard from './Dashboard';
 import NotFound from './NotFound';
+import auth from 'utils/auth';
+
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    const isLoggedIn = auth.loggedIn();
+
+    return (
+        <Route {...rest} render={props => (
+            isLoggedIn ? (
+                <Component {...props}/>
+            ) : (
+                <Redirect to={{
+                    pathname: '/login',
+                    state: { from: props.location }
+                }}/>
+            )
+        )}/>
+    );
+};
 
 const App = () => (
     <BrowserRouter>
@@ -18,7 +38,7 @@ const App = () => (
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/login" component={Login}/>
-                <Route exact path="/dashboard" component={Dashboard}/>
+                <PrivateRoute path="/dashboard" component={Dashboard} />
                 <Route component={NotFound}/>
             </Switch>
         </div>
