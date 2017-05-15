3c61360f3ecfbd612fc8ed2045953d96f99c4b96
Phily Austria <austriaphily@gmail.com>
2017-05-14 22:51:26 -0700

    update(homepage): Updated homepage.

--- src/client/app/components/Home.jsx
+++ src/client/app/components/Home.jsx
@@ -2,14 +2,28 @@
 import React, { Component } from 'react';
 /*eslint-enable no-unused-vars*/
 import { connect } from 'react-redux';
-import RaisedButton from 'material-ui/RaisedButton';
+
+const styles = {
+    jumbotron: {
+        background: 'url("img/landing.jpg") center center',
+        color: 'white',
+        height: '600px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    noMargin: {
+        margin: '0px'
+    }
+};
 
 class Home extends Component {
     render() {
         return(
             <div>
-                This is Home.
-                <RaisedButton label="Default" />
+                <div style={styles.jumbotron}>
+                    <h1 style={styles.noMargin}>Bringing Thais in USA together</h1>
+                </div>
             </div>
         );
     }
