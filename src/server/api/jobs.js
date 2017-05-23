83b12740262de21244135c23946b641b6b73e55c
Phily Austria <austriaphily@gmail.com>
2017-05-22 22:09:29 -0700

    mock(jobs api) : Added jobs mock up data.

--- src/server/api/jobs.js
+++ src/server/api/jobs.js
@@ -22,13 +22,81 @@ app.post('/api/job', (req, res) => {
 
 /**
 * Return a list of jobs.
-// */
-// app.get('/api/jobs', async function (req, res) {
-//     const { name, email, message } = req.body;
-//     try {
-//         const jobs = await JobPosting.find();
-//         res.status(200).send(jobs);
-//     } catch (err) {
-//         res.status(500).send(err.message);
-//     }
-// });
+* TODO: Flag features to report that the job might not available.
+*/
+app.get('/api/jobs', (req, res) => {
+  const jobs = [
+    {
+      title: 'Title 1',
+      location: {
+        city: 'Los Angeles',
+        state: 'CA',
+        zip: '90002'
+      },
+      contact: {
+        phone: null, // Must have at least phone or email.
+        email: null,
+        website: null,
+        name: 'Alex'
+      },
+      status: 'active', // ENUM
+      type: 'restaurant', // ENUM
+      description: 'สุภาพสตรี ผู้ช่วยในครัว',
+      created_date: 1495514492,
+      start_date: 1495514492,
+      expired_date: 1498192891,
+      created_by: 12312 // User ID
+    },
+    {
+      title: 'Title 2',
+      location: {
+        city: 'Los Angeles',
+        state: 'CA',
+        zip: '90002'
+      },
+      contact: {
+        phone: null,
+        email: null,
+        website: null,
+        name: 'Phil'
+      },
+      status: 'inactive',
+      type: 'restaurant',
+      description: 'สุภาพสตรี ผู้ช่วยในครัว sss',
+      created_date: 1495514492,
+      start_date: 1495514492,
+      expired_date: 1498192891,
+      created_by: 12312
+    },
+    {
+      title: 'Title 3',
+      location: {
+        city: 'Encino',
+        state: 'CA',
+        zip: '91316'
+      },
+      contact: {
+        phone: null, //Must have at least phone or email.
+        email: null,
+        website: null,
+        name: 'Ashley'
+      },
+      status: 'active',
+      type: 'nursing', //ENUM
+      description: 'description 3',
+      created_date: 1495514492,
+      start_date: 1495514492,
+      expired_date: 1498192891,
+      created_by: 12312 //User ID
+    }
+  ];
+
+  res.status(200).json({ jobs: jobs });
+    // const { name, email, message } = req.body;
+    // try {
+    //     const jobs = await JobPosting.find();
+    //     res.status(200).send(jobs);
+    // } catch (err) {
+    //     res.status(500).send(err.message);
+    // }
+});
