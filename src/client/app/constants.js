493f3a806dffbe0be20eb8a41d69e80d03233dc9
Phily Austria <austriaphily@gmail.com>
2017-05-23 07:37:30 -0700

    refact(constants) : Added directory prefix.

--- src/client/app/constants.js
+++ src/client/app/constants.js
@@ -1,5 +1,5 @@
-export const CHANGE_FORM = 'CHANGE_FORM';
-export const SET_AUTH = 'SET_AUTH';
-export const SENDING_REQUEST = 'SENDING_REQUEST';
-export const RECEIVE_USER = 'RECEIVE_USER';
-export const LOGOUT = 'LOGOUT';
+export const CHANGE_FORM = 'app/CHANGE_FORM';
+export const SET_AUTH = 'app/SET_AUTH';
+export const SENDING_REQUEST = 'app/SENDING_REQUEST';
+export const RECEIVE_USER = 'app/RECEIVE_USER';
+export const LOGOUT = 'app/LOGOUT';
