fb790bb5a45276c5a29cc72ec982694188672abe
Phily Austria <austriaphily@gmail.com>
2017-05-23 23:16:48 -0700

    wip(jobs) : Called jobs api from jobs page.

--- src/client/app/jobs/components/Jobs.jsx
+++ src/client/app/jobs/components/Jobs.jsx
@@ -2,11 +2,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { compose, lifecycle } from 'recompose';
 /*eslint-enable no-unused-vars*/
 
-const Jobs = () => (
-  <div>This is jobs.</div>
-);
+async function fetchJobs() {
+  const response = await fetch('/api/jobs', {});
+  const res = await response.json();
+  return res.jobs;
+}
+
+const withJobs = lifecycle({
+  state: { jobs: null },
+  async componentDidMount() {
+    const jobs = await fetchJobs();
+    this.setState({jobs: jobs});
+  }
+});
+
+const Jobs = withJobs(({jobs}) => {
+  return (
+    <div>{jobs && jobs.map((job, i) => <div key={i}>{job.description}</div>)} This is jobs.</div>
+  );
+});
 
 const mapStateToProps = state => ({
   state: state
