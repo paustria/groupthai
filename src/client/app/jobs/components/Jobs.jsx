f85c43f0e961044a0ffdef7e1515e87dacb20c37
Phily Austria <austriaphily@gmail.com>
2017-05-25 07:44:43 -0700

    filter(jobs): Showed only active jobs.

--- src/client/app/jobs/components/Jobs.jsx
+++ src/client/app/jobs/components/Jobs.jsx
@@ -2,10 +2,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { compose, lifecycle } from 'recompose';
+import { compose, lifecycle, mapProps } from 'recompose';
 import {Card, CardTitle, CardText} from 'material-ui/Card';
 import Paper from 'material-ui/Paper';
 /*eslint-enable no-unused-vars*/
+
+const styles = {
+  jobType : {float:'right', margin:20}
+};
 
 async function fetchJobs() {
   const response = await fetch('/api/jobs', {});
@@ -21,20 +25,35 @@ const withJobs = lifecycle({
   }
 });
 
+const JobList = ({ jobs, status }) => {
+  return <div>
+    {
+      jobs && jobs.map((job, i) =>
+        (<Card key={i}>
+          <div style={styles.jobType}>{job.type}</div>
+          <CardTitle title={job.title} subtitle={`${job.location.city}, ${job.location.state}`} />
+          <CardText>
+            {job.description}
+          </CardText>
+        </Card>)
+      )
+    }
+  </div>;
+}
+
+const filterByStatus = (status) => mapProps(
+  ({ jobs }) => ({
+    status,
+    jobs: jobs.filter(job => job.status === status)
+  })
+);
+
+const ActiveJobs = filterByStatus('active')(JobList);
+
 const Jobs = withJobs(({jobs}) => {
   return (
     <Paper zDepth={1}>
-      {
-        jobs && jobs.map((job, i) =>
-          (<Card key={i}>
-            <div style={{float:'right', margin:20}}>{job.type}</div>
-            <CardTitle title={job.title} subtitle={`${job.location.city}, ${job.location.state}`} />
-            <CardText>
-              {job.description}
-            </CardText>
-          </Card>)
-        )
-      }
+      { jobs && <ActiveJobs jobs={jobs} /> }
     </Paper>
   );
 });
