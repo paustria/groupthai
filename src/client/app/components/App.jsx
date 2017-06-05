4b3a24a9f78eb3cf850bcd689161f06435f8abbb
Phily Austria <austriaphily@gmail.com>
2017-06-04 21:42:37 -0700

    update(eslint): wip.

--- src/client/app/components/App.jsx
+++ src/client/app/components/App.jsx
@@ -1,41 +1,44 @@
+import 'muicss/dist/css/mui.min.css';
 import React from 'react';
 import {
   Redirect,
   BrowserRouter,
   Switch,
-  Route
+  Route,
 } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // Needed for onTouchTap
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import { connect } from 'react-redux';
-injectTapEventPlugin();
-
-import 'muicss/dist/css/mui.min.css';
-
+import Jobs from 'app/jobs/components/Jobs';
+import { auth } from 'utils/auth';
 import Home from './Home';
 import Nav from './Nav';
 import Login from './Login';
 import Dashboard from './Dashboard';
 import NotFound from './NotFound';
-import Jobs from 'app/jobs/components/Jobs';
-import {auth} from 'utils/auth';
 
+injectTapEventPlugin();
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const isLoggedIn = auth.getAuth();
 
   return (
-    <Route {...rest} render={props => (
-      isLoggedIn ? (
-        <Component {...props}/>
-      ) : (
-        <Redirect to={{
-          pathname: '/login',
-          state: { from: props.location }
-        }}/>
-      )
-    )}/>
+    <Route
+      {...rest}
+      render={props => (
+        isLoggedIn ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location },
+            }}
+          />
+        )
+      )}
+    />
   );
 };
 
@@ -46,11 +49,11 @@ const App = () => (
         <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login}/>
-          <Route exact path="/register" component={Login}/>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Login} />
           <Route exact path="/jobs" component={Jobs} />
           <PrivateRoute path="/dashboard" component={Dashboard} />
-          <Route component={NotFound}/>
+          <Route component={NotFound} />
         </Switch>
       </div>
     </BrowserRouter>
@@ -59,7 +62,7 @@ const App = () => (
 
 
 const mapStateToProps = state => ({
-  state: state
+  state,
 });
 
 export default connect(mapStateToProps)(App);
