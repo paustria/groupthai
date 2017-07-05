a2475f7085d111cb354d12c4382d61feaa2e7a31
Phily Austria <austriaphily@gmail.com>
2017-07-05 09:00:04 -0700

    fix(env) : Fixed MONGODB_URI.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   APP_ROOT: __dirname,
   APP_PORT: process.env.PORT || 3000,
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/groupthai',
-  MONGO_TEST_URI: process.env.MONGO_TEST_URI || 'mongodb://localhost:27017/groupthai_test',
+  MONGO_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/groupthai',
+  MONGO_TEST_URI: process.env.MONGODB_TEST_URI || 'mongodb://localhost:27017/groupthai_test',
 };
